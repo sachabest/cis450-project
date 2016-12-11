17f5be3757a9f60c1a1e047718851a7f390e2f9a
Sacha <sachabest@outlook.com>
2016-12-11 17:52:44 -0500

    missing files

---
+++ public/ts/home.controller.ts
@@ -0,0 +1,10 @@
+namespace cis450.main {
+
+    export class HomeController {
+
+        constructor() {            
+        }
+    }
+
+    getModule().controller("HomeController", HomeController);
+}
